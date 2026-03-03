f8420154975f850fa7b48b84a6c10cc66d7c6a64
bytefennec <bytefennec@gmail.com>
2026-03-04 01:03:54 +0200

    Fix compilation issues

--- namecolor-bukkit/src/main/java/com/pedestriamc/namecolor/manager/BlacklistManager.java
+++ namecolor-bukkit/src/main/java/com/pedestriamc/namecolor/manager/BlacklistManager.java
@@ -1,7 +1,7 @@
 package com.pedestriamc.namecolor.manager;
 
 import com.pedestriamc.namecolor.NameColor;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.jetbrains.annotations.NotNull;
 
