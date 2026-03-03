105da971b764c6e77250e9d7ad287ffefe6b1d1d
bytefennec <bytefennec@gmail.com>
2026-03-04 01:06:25 +0200

    Hijack join and leave messages so they show the nickname instead of profile name

--- namecolor-bukkit/src/main/java/com/pedestriamc/namecolor/listeners/JoinListener.java
+++ namecolor-bukkit/src/main/java/com/pedestriamc/namecolor/listeners/JoinListener.java
@@ -2,6 +2,12 @@ package com.pedestriamc.namecolor.listeners;
 
 import com.pedestriamc.namecolor.NameColor;
 import com.pedestriamc.namecolor.user.UserUtil;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
+
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -18,6 +24,15 @@ public class JoinListener implements Listener {
 
     @EventHandler(priority = EventPriority.LOWEST)
     public void onEvent(@NotNull PlayerJoinEvent event) {
-        userUtil.loadUserAsync(event.getPlayer().getUniqueId());
+        event.joinMessage(null);
+        userUtil.loadUserAsync(event.getPlayer().getUniqueId()).thenRun(() -> {
+            String name = PlainTextComponentSerializer.plainText().serialize(event.getPlayer().playerListName());
+            Component message = Component.text()
+                .append(Component.translatable("multiplayer.player.joined").arguments(Component.text(name)))
+                .color(TextColor.fromHexString(NamedTextColor.YELLOW.asHexString()))
+                .build();
+
+            event.getPlayer().getServer().sendMessage(message);
+        });
     }
 }
--- namecolor-bukkit/src/main/java/com/pedestriamc/namecolor/listeners/LeaveListener.java
+++ namecolor-bukkit/src/main/java/com/pedestriamc/namecolor/listeners/LeaveListener.java
@@ -2,6 +2,12 @@ package com.pedestriamc.namecolor.listeners;
 
 import com.pedestriamc.namecolor.NameColor;
 import com.pedestriamc.namecolor.user.UserUtil;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
+
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
@@ -17,6 +23,15 @@ public class LeaveListener implements Listener {
 
     @EventHandler
     public void onPlayerLeave(@NotNull PlayerQuitEvent event) {
+        event.quitMessage(null);
+        String name = PlainTextComponentSerializer.plainText().serialize(event.getPlayer().playerListName());
+        Component message = Component.text()
+            .append(Component.translatable("multiplayer.player.left").arguments(Component.text(name)))
+            .color(TextColor.fromHexString(NamedTextColor.YELLOW.asHexString()))
+            .build();
+
+        event.getPlayer().getServer().sendMessage(message);
+
         userUtil.removeUser(event.getPlayer().getUniqueId());
     }
 }
